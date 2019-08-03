b2553168e1f1f71009f053aa42d47d435a84d6d2
Frank Serra <frankpserra@gmail.com>
2019-08-03 14:32:48 -0400

    Much improved chargen

--- randomgen.rb
+++ randomgen.rb
@@ -40,145 +40,153 @@ class Character
     out += frame
     return out
   end
-end
 
-char = Character.new()
+  def initialize
+    db = SQLite3::Database.open 'db/development.sqlite3'
 
-db = SQLite3::Database.open 'db/development.sqlite3'
+    #Config
+    config = (db.query 'SELECT statlinemax, elementdoublepct FROM char_configs LIMIT 1').next
 
-#Config
-config = (db.query 'SELECT statlinemax, elementdoublepct FROM char_configs LIMIT 1').next
+    #Race
+    race = (db.query 'SELECT ID, NAME from races ORDER BY RANDOM() LIMIT 1').next
+    @race = race[1]
 
-#Race
-race = (db.query 'SELECT ID, NAME from races ORDER BY RANDOM() LIMIT 1').next
-char.race = race[1]
+    #Stats
+    stats = db.query 'SELECT ID, NAME, MINVAL, MAXVAL from stats ORDER BY ID'
+    statline = {}
+    statline_total = 0
 
-#Stats
-stats = db.query 'SELECT ID, NAME, MINVAL, MAXVAL from stats ORDER BY ID'
-statline = {}
-statline_total = 0
-
-loop do
-  statline_okay = true
-  statline = {}
-  statline_total = 0
-  stats.each do |stat|
-    roll = rand(stat[2]..stat[3])
-    statline[stat[1]] = roll.to_s
-    statline_total += roll
-    if(statline_total > config[0])
-      statline_okay = false
+    loop do
+      statline_okay = true
+      statline = {}
+      statline_total = 0
+      stats.each do |stat|
+        roll = rand(stat[2]..stat[3])
+        statline[stat[1]] = roll.to_s
+        statline_total += roll
+        if(statline_total > config[0])
+          statline_okay = false
+        end
+      end
+      stats.reset
+    break if statline_okay == true
     end
-  end
-  stats.reset
-break if statline_okay == true
-end
 
-char.stats = statline
+    @stats = statline
 
-#Choose and calculate resource
-resource = (db.query 'SELECT ID, NAME, BASEVAL, ADDSTATLINE, SUBTRACTSTATLINE, GENERATESPELL from resources ORDER BY RANDOM() LIMIT 1').next
-resource_pool = resource[2]
-statexcess = (config[0] - statline_total)
-if resource[3] #base add statline
-  resource_pool += statexcess
-  if resource_pool < resource[2]
+    #Choose and calculate resource
+    resource = (db.query 'SELECT ID, NAME, BASEVAL, ADDSTATLINE, SUBTRACTSTATLINE, GENERATESPELL from resources ORDER BY RANDOM() LIMIT 1').next
     resource_pool = resource[2]
-  end
-elsif resource[4] #base minus statline
-  resource_pool -= statexcess
-  if resource_pool > resource[2]
-    resource_pool = resource[2]
-  end
-end
-
-char.resourcename  = resource[1]
-char.resourcevalue = resource_pool.to_s
-
-#Style
-style = (db.query 'SELECT offense_types.ID, offense_types.NAME, offense_types.NUMBEROFWEAPONS from offense_types INNER JOIN resource_loadout_combos ON offense_types.ID = resource_loadout_combos.offense_type_id AND resource_loadout_combos.resource_id = ' + resource[0].to_s + ' ORDER BY RANDOM() LIMIT 1').next
-if style != nil
-  char.style = style[1]
-end
-
-#Weapons
-if style != nil
-  mandatory_weapons = db.query 'SELECT weapons.ID, weapons.NAME from weapons INNER JOIN weapon_offense_type_combos ON weapon_offense_type_combos.alwayspick = 1 AND weapon_offense_type_combos.weapon_id = weapons.id AND weapon_offense_type_combos.offense_type_id = ' + style[0].to_s
-  weapons = db.query 'SELECT weapons.ID, weapons.NAME from weapons INNER JOIN weapon_offense_type_combos ON weapon_offense_type_combos.weapon_id = weapons.id AND weapon_offense_type_combos.offense_type_id = ' + style[0].to_s + ' ORDER BY RANDOM() LIMIT ' + style[2].to_s
-
-  loadout = []
-  mandatory_weapons.each do |w|
-    loadout.append(w[1])
-  end
-  weapons.each do |w|
-    loadout.append(w[1])
-  end
-
-  if loadout.count < style[2]
-    loadout.append(loadout[-1])
-  end
-
-  char.loadout = loadout
-end
-
-#Armor
-armor = (db.query 'SELECT ID, NAME from armor_types ORDER BY RANDOM() LIMIT 1').next
-char.armor = armor[1]
-
-#Spell
-spell = ''
-  #if Resource generates a spell
-  if resource[5] == 1
-    number_shapes = 1
-
-#Modifier, then check if we get 2 shapes instead
-  modifier = (db.query 'SELECT ID, NAME, REPLACEWITHSHAPE from modifiers ORDER BY RANDOM() LIMIT 1').next
-  if modifier[2] == 1
-    number_shapes += 1
-  else
-    spell += modifier[1] + ' '
-  end
-
-#Shape
-  shape_after_text = ''
-  shapes = db.query 'SELECT ID, NAME, AFTERELEMENT from shapes ORDER BY RANDOM(), ORDERING LIMIT '+number_shapes.to_s
-
-  shapes.each do |shape|
-    if shape[2] == 0
-      spell += shape[1]
-      spell += ' '
-    else
-      shape_after_text += shape[1]
-      shape_after_text += ' '
+    statexcess = (config[0] - statline_total)
+    if resource[3] #base add statline
+      resource_pool += statexcess
+      if resource_pool < resource[2]
+        resource_pool = resource[2]
+      end
+    elsif resource[4] #base minus statline
+      resource_pool -= statexcess
+      if resource_pool > resource[2]
+        resource_pool = resource[2]
+      end
     end
-  end
 
-#Elements
-  number_elements = 1
+    @resourcename  = resource[1]
+    @resourcevalue = resource_pool.to_s
 
-  #roll chance of double elements
-  if rand(1..100) <= config[1]
-    number_elements += 1
-  end
-
-  element_text = ''
-  number_elements.times do
-    element = (db.query 'SELECT ID, NAME from elements ORDER BY RANDOM() LIMIT 1').next
-    if element_text != ''
-      element_text += '+'
+    #Style
+    style = (db.query 'SELECT offense_types.ID, offense_types.NAME, offense_types.NUMBEROFWEAPONS from offense_types INNER JOIN resource_loadout_combos ON offense_types.ID = resource_loadout_combos.offense_type_id AND resource_loadout_combos.resource_id = ' + resource[0].to_s + ' ORDER BY RANDOM() LIMIT 1').next
+    if style != nil
+      @style = style[1]
     end
-    element_text += element[1]
-  end
 
-  spell += element_text
+    #Weapons
+    if style != nil
+      mandatory_weapons = db.query 'SELECT weapons.ID, weapons.NAME from weapons INNER JOIN weapon_offense_type_combos ON weapon_offense_type_combos.alwayspick = 1 AND weapon_offense_type_combos.weapon_id = weapons.id AND weapon_offense_type_combos.offense_type_id = ' + style[0].to_s
+      weapons = db.query 'SELECT weapons.ID, weapons.NAME from weapons INNER JOIN weapon_offense_type_combos ON weapon_offense_type_combos.weapon_id = weapons.id AND weapon_offense_type_combos.offense_type_id = ' + style[0].to_s + ' ORDER BY RANDOM() LIMIT ' + style[2].to_s
 
-  if shape_after_text != ''
-    spell += ' ' + shape_after_text
+      loadout = []
+      mandatory_weapons.each do |w|
+        loadout.append(w[1])
+      end
+      weapons.each do |w|
+        loadout.append(w[1])
+      end
+
+      if loadout.count < style[2]
+        loadout.append(loadout[-1])
+      end
+
+      @loadout = loadout
+    end
+
+    #Armor
+    armor = (db.query 'SELECT ID, NAME from armor_types ORDER BY RANDOM() LIMIT 1').next
+    @armor = armor[1]
+
+    #Spell
+    spell = ''
+      #if Resource generates a spell
+      if resource[5] == 1
+        number_shapes = 1
+
+    #Modifier, then check if we get 2 shapes instead
+      modifier = (db.query 'SELECT ID, NAME, REPLACEWITHSHAPE from modifiers ORDER BY RANDOM() LIMIT 1').next
+      if modifier[2] == 1
+        number_shapes += 1
+      else
+        spell += modifier[1] + ' '
+      end
+
+    #Shape
+      shape_after_text = ''
+      shapes = db.query 'SELECT ID, NAME, AFTERELEMENT from shapes ORDER BY RANDOM(), ORDERING LIMIT '+number_shapes.to_s
+
+      shapes.each do |shape|
+        if shape[2] == 0
+          spell += shape[1]
+          spell += ' '
+        else
+          shape_after_text += shape[1]
+          shape_after_text += ' '
+        end
+      end
+
+    #Elements
+      number_elements = 1
+
+      #roll chance of double elements
+      if rand(1..100) <= config[1]
+        number_elements += 1
+      end
+
+      element_text = ''
+      number_elements.times do
+        element = (db.query 'SELECT ID, NAME from elements ORDER BY RANDOM() LIMIT 1').next
+        if element_text != ''
+          element_text += '+'
+        end
+        element_text += element[1]
+      end
+
+      spell += element_text
+
+      if shape_after_text != ''
+        spell += ' ' + shape_after_text
+      end
+    end
+    @spell = spell
+
+    #Ability
+    #not implemented yet
   end
 end
-char.spell = spell
 
-#Ability
-#not implemented yet
+looper = 5
+if ARGV.length > 0
+  looper = ARGV[0]
+end
 
-puts char.output
+looper.to_i.times do
+  char = Character.new()
+  puts char.output
+end #repeat for generation
